22d90ecc27995a4574da1131d1d9acc3e0b3aafa
sti4er <katleta1802@mail.ru>
2015-07-26 14:05:33 +0300

    boilerplate

---
+++ assets/app/vendor/all.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	outdatedBrowser({
+		bgColor: '#f25648',
+		color: '#ffffff',
+		lowerThan: 'transform',
+		languagePath: '/app/vendor/outdatedbrowser/lang/ru.html'
+	})
+});
---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+"use strict"; // Помогает увидеть где ошибка.
+
+var gulp = require('gulp'), // Вызов функции gulp по умолчанию.
+concatCss = require('gulp-concat-css'), // Склейка файлов. https://www.npmjs.com/package/gulp-concat-css
+autoprefixer = require('gulp-autoprefixer'), // Авто префиксер. https://www.npmjs.com/package/gulp-autoprefixer
+livereload = require('gulp-livereload'), // https://www.npmjs.com/package/gulp-livereload
+jade = require('gulp-jade'), // Плагин для Jade
+stylus = require('gulp-stylus'), // Плагин для Stylus
+browserSync = require('browser-sync'),
+newer = require('gulp-newer'),
+plumber = require('gulp-plumber'),// Проверка на ощибки
+reload = browserSync.reload;
+
+//All browserSync
+gulp.task('server', function() {
+  browserSync({
+    server: {
+      baseDir: "public"
+    }
+  });
+});
+
+
+// Complite STYLUS and automatically Prefix CSS
+gulp.task('stylus', function() {
+  return gulp.src(['assets/app/**/*.styl', '!assets/**/_*.styl'])
+            .pipe(stylus())
+            .pipe(plumber())
+        .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+        .pipe(gulp.dest('public/app'))
+        .pipe(browserSync.reload({stream: true}));
+    });
+
+//Complite html
+gulp.task('jade', function() {
+  return gulp.src(['assets/**/*.jade', '!assets/**/_*.jade', '!assets/pages/index.jade', '!./assets/pages/blocks.jade'])
+            .pipe(jade({
+              pretty: true,
+              basedir: 'assets'
+            }))
+          .pipe(plumber())
+          .on('error', console.error.bind(console))
+          .pipe(gulp.dest('./public/'))
+          .pipe(browserSync.reload({stream: true}));
+          });
+
+//creat index.html
+gulp.task('index', function() {
+  return gulp.src('assets/pages/index.jade')
+            .pipe(jade({
+              pretty: true,
+              basedir: 'assets'
+            }))
+          .pipe(plumber())
+          .on('error', console.error.bind(console))
+          .pipe(gulp.dest('public'))
+          .pipe(browserSync.reload({stream: true}));
+          });
+
+//Blocks
+gulp.task('blocks', ['jade'], function() {
+  return gulp.src('./assets/pages/blocks.jade')
+            .pipe(jade({
+              pretty: true,
+              basedir: 'assets'
+            }))
+          .pipe(plumber())
+          .on('error', console.error.bind(console))
+          .pipe(gulp.dest('./public/pages/'))
+          .pipe(browserSync.reload({stream: true}));
+          });
+
+// Copy All Files At (images)
+gulp.task('images', function() {
+  return gulp.src(['assets/img/**', '!assets/img/svg', '!assets/img/png'])
+          .pipe(newer('public/img'))
+          .pipe(gulp.dest('public/img'))
+          .pipe(browserSync.reload({stream: true}));
+      });
+
+gulp.task('_images', function() {
+  return gulp.src('assets/_img/**')
+          .pipe(newer('public/_img'))
+          .pipe(gulp.dest('public/_img'))
+          .pipe(browserSync.reload({stream: true}));
+      });
+
+// Copy All Files At (app)
+gulp.task('app', function() {
+  return gulp.src(['assets/app/vendor/*.*', 'assets/app/vendor/**/*.*'])
+          .pipe(newer('public/app/vendor'))
+          .pipe(gulp.dest('public/app/vendor'))
+          .pipe(browserSync.reload({stream: true}));
+      });
+
+// Copy Web Fonts To Dist
+gulp.task('font', function() {
+  return gulp.src('assets/font/**')
+          .pipe(newer('public/font'))
+          .pipe(gulp.dest('public/font'))
+          .pipe(browserSync.reload({stream: true}));
+      });
+
+// Watch everything
+gulp.task('watch', function() {
+  gulp.watch('assets/blocks/**/*.styl',['stylus']);
+  gulp.watch('assets/blocks/**/*.jade', ['jade']);
+  gulp.watch('assets/app/vendor/**/*.*', ['app']);
+  gulp.watch('assets/img/**', ['images']);
+  gulp.watch('assets/_img/**', ['_images']);
+  gulp.watch('assets/font/*', ['font']);
+  gulp.watch('assets/app/_*.styl', ['stylus']);
+  gulp.watch('assets/pages/*',['blocks']);
+  gulp.watch('assets/pages/index.jade', ['index']);
+  gulp.watch('assets/pages/_*.jade', ['jade']);
+});
+
+
+gulp.task('default', ['jade', 'stylus', 'app', 'images', 'font', '_images', 'index', 'server', 'blocks', 'watch']);
