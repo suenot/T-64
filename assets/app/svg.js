140273e16dbf3d433144af59d7612a6de1c1bd90
sti4er <katleta1802@mail.ru>
2015-10-04 00:22:06 +0300

    svg.jquery -> svg.js

--- assets/app/svg.js
+++ assets/app/svg.js
@@ -1,5 +1,21 @@
-$('document').ready(function(){
-	$('.isvg').each(function(){
-		$(this).load('/img/svg/' + $(this).data('icon') + '.svg').addClass($(this).data('icon'));
+// $('document').ready(function(){
+// 	$('.isvg').each(function(){
+// 		$(this).load('/img/svg/' + $(this).data('icon') + '.svg').addClass($(this).data('icon'));
+// 	});
+// });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var icons = document.getElementsByClassName('isvg');
+	// console.log(icons);
+	Array.prototype.forEach.call(icons, function(icon, i){
+		// var icon = icons[i];
+		console.log(icon);
+		var http = new XMLHttpRequest();
+		http.open('GET', '/img/svg/' + icon.getAttribute('data-icon') + '.svg', true);
+		icon.className = icon.className + ' ' + icon.getAttribute('data-icon');
+		http.onload = function () {
+			icon.innerHTML = http.responseText;
+		}
+		http.send();
 	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -291,7 +291,7 @@ gulp.task('del', function () {
 gulp.task('watch', function() {
   gulp.watch('assets/blocks/**/*.styl',['stylus']);
   gulp.watch('assets/blocks/**/*.jade', ['jade']);
-  gulp.watch('assets/app/vendor/**/*.*', ['app']);
+  gulp.watch('assets/app/**/**/*.*', ['app']);
   gulp.watch('assets/img/**', ['images']);
   gulp.watch('assets/_img/**', ['_images']);
   gulp.watch('assets/font/*', ['font']);
