c73eb2de97d78257b94b29cc6442c05d7ecbf1b3
Eugen Soloviov <suenot@gmail.com>
2015-10-04 22:09:55 +0400

    fix del

--- gulp/default/clean.js
+++ gulp/default/clean.js
@@ -1,7 +1,6 @@
 // Delete in public all files before start gulp
 var gulp = require('gulp');
 var del = require('del');
-var rmdir = require('rmdir');
 var vinylPaths = require('vinyl-paths');
 
 // Deleted folder public
