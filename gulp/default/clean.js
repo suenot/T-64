8baf4b215dba75971782031b44a2fd1f9e607979
Eugen Soloviov <suenot@gmail.com>
2015-10-04 22:09:49 +0400

    fix del

--- gulp/default/clean.js
+++ gulp/default/clean.js
@@ -5,12 +5,6 @@ var rmdir = require('rmdir');
 var vinylPaths = require('vinyl-paths');
 
 // Deleted folder public
-gulp.task('del', function () {
-	return gulp.src(['public/*', '!public/CNAME', '!public/.git'])
-	.pipe(vinylPaths(del));
+gulp.task('del', function() {
+	del(['public/*', '!public/CNAME', '!public/.git']);
 });
-
-// Deleted Libs.min.css and app.min.css in folder public/app
-// gulp.task('delmin', function (cb) {
-// 	del(['public/app/*.min.css'], cb)
-// })
