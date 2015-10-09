fd4d7366db3bbeef43b49c5f7fa3924ab97ab4fe
Eugen Soloviov <suenot@gmail.com>
2015-10-09 14:28:26 +0400

    fix del

--- gulp/default/clean.js
+++ gulp/default/clean.js
@@ -1,8 +1,9 @@
 // Delete in public all files before start gulp
 var gulp = require('gulp');
-var del = require('del');
+var rimraf = require('gulp-rimraf');
 
 // Deleted folder public
-gulp.task('del', function(cb) {
-	del(['public/*', '!public/CNAME', '!public/.git'], cb);
+gulp.task('del', function() {
+	return gulp.src(['public/*', '!public/CNAME', '!public/.git'], { read: false })
+	.pipe(rimraf({ force: true }))
 });
